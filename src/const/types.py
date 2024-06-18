c50b4d6bba98f8850d0a14e6f45fab6a37aa79c6
weeger <romain.weeger@gmail.com>
2024-06-18 20:40:35 +0200

    Various rewrite

---
+++ src/const/types.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+from typing import Union
+
+FileStringOrPath = Union[str, Path]
--- src/file_state_manager.py
+++ src/file_state_manager.py
@@ -1,39 +1,41 @@
+from pathlib import Path
 from typing import Optional
 
 import yaml
 from pydantic import BaseModel
 
+from src.const.types import FileStringOrPath
+from src.helpers.file_helper import file_resolve_path
 from src.result.file_state_dry_run_result import FileStateDryRunResult
+from src.result.file_state_result import FileStateResult
 
 
 class FileStateManager(BaseModel):
-    root_directory: str
-    _config: Optional[dict] = None
+    root_directory: Path
+    config: dict = {}
 
-    @property
-    def config(self) -> Optional[dict]:
-        return self._config
+    def __init__(self, root_directory: FileStringOrPath, config: Optional[dict] = None):
+        super().__init__(
+            root_directory=file_resolve_path(root_directory)
+        )
 
-    @config.setter
-    def config(self, value: dict):
-        self._config = value
+        if config:
+            self.configure(config)
 
     def configure(self, config: dict):
         self.config = config
 
-    def configure_from_file(self, file_path: str):
-        with open(file_path, 'r') as file:
-            self.config = yaml.safe_load(file)
+    def configure_from_file(self, path: FileStringOrPath):
+        path = file_resolve_path(path)
+        with open(path, 'r') as f:
+            self.config = yaml.safe_load(f)
 
-    def dry_run(self):
-        # Implement dry run logic here
-        # This function should return an object that can be printed to show what changes would be made
-        return FileStateDryRunResult(self.config)
+    def dry_run(self) -> FileStateDryRunResult:
+        return FileStateDryRunResult()
 
-    def succeed(self):
+    def succeed(self) -> bool:
         # Implement logic to check if the configuration can be successfully applied
         return True  # Return True if successful, False otherwise
 
-    def apply(self):
-        # Implement the logic to apply the configuration here
-        pass
+    def apply(self) -> FileStateResult:
+        return FileStateResult()
