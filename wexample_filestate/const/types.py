a286c7c5a7ec748704fff26441db96aa8094f367
weeger <romain.weeger@gmail.com>
2024-06-19 18:40:40 +0200

    Type

--- wexample_filestate/const/types.py
+++ wexample_filestate/const/types.py
@@ -1,4 +1,13 @@
-from typing import TypedDict
+from __future__ import annotations
+
+from typing import Optional
+
+from typing_extensions import TypedDict
+
+
+class StateItemConfig(TypedDict):
+    name: str
+    children: Optional[any]
 
 
 class FileSystemStructurePermission(TypedDict):
