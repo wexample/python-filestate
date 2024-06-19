13ce286fa99cdf387593f122be621b5a2f8f136e
weeger <romain.weeger@gmail.com>
2024-06-19 18:49:19 +0200

    Style

--- wexample_filestate/item/file_state_item_directory_target.py
+++ wexample_filestate/item/file_state_item_directory_target.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
-from typing import Optional
-
+from wexample_filestate.const.types import StateItemConfig
+from wexample_filestate.helpers.state_item_helper import state_item_target_from_path
 from wexample_filestate.item.file_state_item_directory import FileStateItemDirectory
-from wexample_filestate.item.file_state_item_file_target import FileStateItemFileTarget
 from wexample_filestate.item.mixins.state_item_target_mixin import StateItemTargetMixin
 
 
-from wexample_filestate.const.types import StateItemConfig
-from wexample_filestate.helpers.state_item_helper import state_item_target_from_path
-
 class FileStateItemDirectoryTarget(FileStateItemDirectory, StateItemTargetMixin):
-    config: Optional[dict] = None
+    config: StateItemConfig = None
 
     def configure(self, config: dict):
         base_path = self.get_resolved()
