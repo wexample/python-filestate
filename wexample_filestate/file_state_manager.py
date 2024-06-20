2eb0729d2df236caf18031f6e97f7ea74f56bb09
weeger <romain.weeger@gmail.com>
2024-06-20 16:50:23 +0200

    Create target

--- wexample_filestate/file_state_manager.py
+++ wexample_filestate/file_state_manager.py
@@ -1,8 +1,8 @@
-from typing import cast, Union, Optional
+from typing import cast, Optional
 
 from pydantic import BaseModel, Field
 
-from wexample_filestate.const.types_state_items import SourceFileOrDirectory
+from wexample_filestate.const.types_state_items import SourceFileOrDirectory, TargetFileOrDirectory
 from wexample_filestate.helpers.state_item_helper import state_item_source_from_path
 from wexample_filestate.result.abstract_result import AbstractResult
 from wexample_filestate.result.file_state_dry_run_result import FileStateDryRunResult
@@ -10,11 +10,15 @@ from wexample_filestate.result.file_state_dry_run_result import FileStateDryRunR
 
 class FileStateManager(BaseModel):
     root: SourceFileOrDirectory = Field(..., description="Current root item definition")
+    _target: TargetFileOrDirectory = None
 
     def __init__(self, root: str, config: Optional[dict] = None):
         super().__init__(root=state_item_source_from_path(root))
 
         self._target = self.root.create_target()
+
+        if config:
+            self.configure(config)
 
     def configure(self, config: dict):
         pass
