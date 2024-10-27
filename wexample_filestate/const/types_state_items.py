03952ab5e1f0a1b6e653084eb9901ad60cae683b
weeger <romain.weeger@gmail.com>
2024-10-27 14:21:17 +0100

    Type import

---
+++ wexample_filestate/const/types_state_items.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Union, TYPE_CHECKING
+
+from wexample_helpers.helpers.import_helper import import_dummy
+
+if TYPE_CHECKING:
+    from wexample_filestate.item.file_state_item_directory_target import FileStateItemDirectoryTarget
+
+import_dummy(FileStateItemDirectoryTarget)
+
+TargetFileOrDirectory = Union["FileStateItemDirectoryTarget", "FileStateItemFileTarget"]
+SourceFileOrDirectory = Union["FileStateItemDirectorySource", "FileStateItemFileSource"]
