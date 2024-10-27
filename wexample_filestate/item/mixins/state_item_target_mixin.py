447e6b7c2123a82c4ddf8bf555ab7b1f08dd8bed
weeger <romain.weeger@gmail.com>
2024-10-27 14:09:56 +0100

    Minimal config

--- wexample_filestate/item/mixins/state_item_target_mixin.py
+++ wexample_filestate/item/mixins/state_item_target_mixin.py
@@ -7,3 +7,17 @@ from wexample_config.const.types import StateItemConfig
 
 class StateItemTargetMixin(BaseModel):
     config: Optional[StateItemConfig] = None
+
+    def __init__(self, config: Optional[StateItemConfig] = None, **data):
+        super().__init__(**data)
+
+        config = self.build_config(config)
+
+        if config:
+            self.configure(config)
+
+    def build_config(self, config: Optional[StateItemConfig] = None) -> StateItemConfig:
+        return config or {}
+
+    def configure(self, config: Optional[StateItemConfig]) -> None:
+        pass
