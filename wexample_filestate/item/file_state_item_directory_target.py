b820618e3994db3e721761e2c764ddfa421d9cf2
weeger <romain.weeger@gmail.com>
2024-10-27 13:52:16 +0100

    Use external config

--- wexample_filestate/item/file_state_item_directory_target.py
+++ wexample_filestate/item/file_state_item_directory_target.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from typing import Optional
+
+from wexample_config.const.types import StateItemConfig
 from wexample_filestate.item.file_state_item_directory import FileStateItemDirectory
 from wexample_filestate.item.mixins.state_item_target_mixin import StateItemTargetMixin
 
@@ -9,6 +12,9 @@ class FileStateItemDirectoryTarget(FileStateItemDirectory, StateItemTargetMixin)
     def create_from_path(
         cls,
         path: str,
+        config: Optional[StateItemConfig] = None,
     ) -> FileStateItemDirectoryTarget:
+        config = config or {}
+
         return cls(
         )
