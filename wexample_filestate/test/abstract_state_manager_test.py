d2cab13824e674e450ae8c9a7a90bdeae24d90a0
weeger <romain.weeger@gmail.com>
2024-10-28 06:52:17 +0100

    Path

--- wexample_filestate/test/abstract_state_manager_test.py
+++ wexample_filestate/test/abstract_state_manager_test.py
@@ -9,10 +9,16 @@ if TYPE_CHECKING:
 class AbstractStateManagerTest(unittest.TestCase):
     state_manager: "FileStateManager"
 
+    def get_package_root_path(self) -> str:
+        return os.path.realpath(os.path.dirname(__file__) + '/../../') + os.sep
+
+    def get_package_resources_path(self) -> str:
+        return os.path.join(
+            self.get_package_root_path(), 'tests', 'resources'
+        ) + os.sep
+
     def setUp(self) -> None:
         from wexample_filestate.file_state_manager import FileStateManager
 
-        current_package_path = os.path.realpath(os.path.dirname(__file__) + '/../../')
         self.state_manager = FileStateManager.create_from_path(
-            path=os.path.join(
-                current_package_path, 'tests', 'resources'))
+            path=self.get_package_resources_path())
