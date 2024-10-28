af451fb66848be5dbdbda8d83eacad7a42cecb01
weeger <romain.weeger@gmail.com>
2024-10-28 09:51:12 +0100

    Stricter type

--- wexample_filestate/option/mode_option.py
+++ wexample_filestate/option/mode_option.py
@@ -1,26 +1,24 @@
-from typing import Type, Union
-from types import UnionType
+from typing import Union
+
 from wexample_filestate.const.files import FileSystemPermission
 from wexample_config.option.abstract_option import AbstractOption
 from wexample_helpers.helpers.file_helper import file_mode_octal_to_num
 
 
 class ModeOption(AbstractOption):
-    value: FileSystemPermission
-
     @staticmethod
-    def get_value_type() -> Type | UnionType:
-        return Union[str, int]
+    def get_value_type() -> FileSystemPermission:
+        return Union[str, int, FileSystemPermission]
 
     def get_octal(self) -> str:
-        if isinstance(self.value, str):
-            return self.value
-        elif isinstance(self.value, int):
-            return str(self.value)
+        if self.value.is_str():
+            return self.value.get_str()
+        elif self.value.is_int():
+            return self.value.to_str()
         elif isinstance(self.value, FileSystemPermission) and 'mode' in self.value:
             return str(self.value['mode'])
         else:
-            raise ValueError('Invalid input')
+            raise ValueError(f"Unexpected value in get_octal: {self.value}")
 
     def get_int(self) -> int:
         return file_mode_octal_to_num(self.get_octal())
