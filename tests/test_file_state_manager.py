01ec8716a44fca071886569a5e705cbad7dff3b4
weeger <romain.weeger@gmail.com>
2024-10-30 15:38:20 +0100

    New test

--- tests/test_file_state_manager.py
+++ tests/test_file_state_manager.py
@@ -1,3 +1,6 @@
+import os
+
+import pytest
 from wexample_filestate.test.abstract_state_manager_test import AbstractStateManagerTest
 
 
@@ -7,3 +10,9 @@ class TestFileStateManager(AbstractStateManagerTest):
 
     def test_configure_name(self):
         self.state_manager.set_value({"name": "yes"})
+
+    def test_configure_unexpected(self):
+        from wexample_config.exception.option import InvalidOptionException
+
+        with pytest.raises(InvalidOptionException):
+            self.state_manager.set_value({"unexpected_option": "yes"})
