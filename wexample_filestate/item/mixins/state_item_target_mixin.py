73c59bf7e74bf66a817585693638afd0185ab9af
weeger <romain.weeger@gmail.com>
2024-10-30 15:33:26 +0100

    Style

--- wexample_filestate/item/mixins/state_item_target_mixin.py
+++ wexample_filestate/item/mixins/state_item_target_mixin.py
@@ -1,22 +1,25 @@
-from typing import List, Type, TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Type
 
 from wexample_config.config_option.abstract_nested_config_option import (
     AbstractNestedConfigOption,
 )
 
-
 if TYPE_CHECKING:
-    from wexample_config.options_provider.abstract_options_provider import AbstractOptionsProvider
+    from wexample_config.options_provider.abstract_options_provider import (
+        AbstractOptionsProvider,
+    )
+
 
 class StateItemTargetMixin(AbstractNestedConfigOption):
-
 
     def get_options_providers(self) -> List[Type["AbstractOptionsProvider"]]:
         providers = super().get_options_providers()
         if len(providers) > 0:
             return providers
 
-        from wexample_filestate.options_provider.default_options_provider import DefaultOptionsProvider
+        from wexample_filestate.options_provider.default_options_provider import (
+            DefaultOptionsProvider,
+        )
 
         return [
             DefaultOptionsProvider,
--- wexample_filestate/options_provider/default_options_provider.py
+++ wexample_filestate/options_provider/default_options_provider.py
@@ -1,26 +1,30 @@
-from typing import List, TYPE_CHECKING, Type
+from typing import TYPE_CHECKING, List, Type
 
-from wexample_config.options_provider.abstract_options_provider import AbstractOptionsProvider
+from wexample_config.options_provider.abstract_options_provider import (
+    AbstractOptionsProvider,
+)
 
 if TYPE_CHECKING:
-    from wexample_config.config_option.abstract_config_option import AbstractConfigOption
+    from wexample_config.config_option.abstract_config_option import (
+        AbstractConfigOption,
+    )
 
 
 class DefaultOptionsProvider(AbstractOptionsProvider):
     @classmethod
     def get_options(cls) -> List[Type["AbstractConfigOption"]]:
-        from wexample_filestate.option.class_config_option import ClassConfigOption
-        from wexample_filestate.option.content_config_option import ContentConfigOption
-        from wexample_filestate.option.name_pattern_config_option import NamePatternConfigOption
-        from wexample_config.option.children_config_option import ChildrenConfigOption
-        from wexample_filestate.option.default_content_config_option import DefaultContentConfigOption
-        from wexample_filestate.option.mode_config_option import ModeConfigOption
-        from wexample_filestate.option.mode_recursive_config_option import ModeRecursiveConfigOption
-        from wexample_config.option.name_config_option import NameConfigOption
-        from wexample_filestate.option.should_exist_config_option import ShouldExistConfigOption
-        from wexample_filestate.option.type_config_option import TypeConfigOption
-        from wexample_filestate.option.remove_backup_max_file_size_config_option import RemoveBackupMaxFileSizeConfigOption
+        # from wexample_filestate.config_option.class_config_option import ClassConfigOption
+        # from wexample_filestate.config_option.content_config_option import ContentConfigOption
+        # from wexample_filestate.config_option.name_pattern_config_option import NamePatternConfigOption
+        # from wexample_filestate.config_option.default_content_config_option import DefaultContentConfigOption
+        # from wexample_filestate.config_option.mode_config_option import ModeConfigOption
+        # from wexample_filestate.config_option.mode_recursive_config_option import ModeRecursiveConfigOption
+        # from wexample_filestate.config_option.should_exist_config_option import ShouldExistConfigOption
+        # from wexample_filestate.config_option.type_config_option import TypeConfigOption
+        # from wexample_filestate.config_option.remove_backup_max_file_size_config_option import RemoveBackupMaxFileSizeConfigOption
         from wexample_config.config_option.name_config_option import NameConfigOption
+
+        # from wexample_config.option.children_config_option import ChildrenConfigOption
 
         return [
             ChildrenConfigOption,
