3b0876dd3fc1656d2db77b6a66ce55b7bd7f105e
weeger <romain.weeger@gmail.com>
2024-10-31 10:04:15 +0100

    Dead code

--- wexample_filestate/protocol/has_target_protocol.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol
-
-from wexample_filestate.const.types_state_items import TargetFileOrDirectory
-
-
-class HasTargetProtocol(Protocol):
-    target: TargetFileOrDirectory
