94c35bb28ef4b9fdc08faeea8611e6830e26ca28
weeger <romain.weeger@gmail.com>
2024-10-31 09:12:26 +0100

    First operation test passed

--- wexample_filestate/options_provider/default_options_provider.py
+++ wexample_filestate/options_provider/default_options_provider.py
@@ -11,28 +11,20 @@ if TYPE_CHECKING:
 class DefaultOptionsProvider(AbstractOptionsProvider):
     @classmethod
     def get_options(cls) -> List[Type["AbstractConfigOption"]]:
-        from wexample_filestate.config_option.children_config_option import ChildrenConfigOption
-        from wexample_filestate.config_option.class_config_option import ClassConfigOption
-        from wexample_config.config_option.name_config_option import NameConfigOption
-        from wexample_filestate.config_option.children_config_option import (
-            ChildrenConfigOption,
-        )
-        from wexample_filestate.config_option.class_config_option import (
-            ClassConfigOption,
-        )
-
-        from wexample_config.config_option.name_config_option import \
-            NameConfigOption
         from wexample_filestate.config_option.children_config_option import \
             ChildrenConfigOption
         from wexample_filestate.config_option.class_config_option import \
             ClassConfigOption
+        from wexample_filestate.config_option.content_config_option import ContentConfigOption
         from wexample_filestate.config_option.default_content_config_option import \
             DefaultContentConfigOption
-        # from wexample_filestate.config_option.content_config_option import ContentConfigOption
-        # from wexample_filestate.config_option.name_pattern_config_option import NamePatternConfigOption
-        # from wexample_filestate.config_option.mode_config_option import ModeConfigOption
-        # from wexample_filestate.config_option.mode_recursive_config_option import ModeRecursiveConfigOption
+        from wexample_filestate.config_option.mode_config_option import ModeConfigOption
+        from wexample_filestate.config_option.mode_recursive_config_option import ModeRecursiveConfigOption
+        from wexample_config.config_option.name_config_option import \
+            NameConfigOption
+        from wexample_filestate.config_option.name_pattern_config_option import NamePatternConfigOption
+        from wexample_filestate.config_option.remove_backup_max_file_size_config_option import \
+            RemoveBackupMaxFileSizeConfigOption
         from wexample_filestate.config_option.should_exist_config_option import \
             ShouldExistConfigOption
         from wexample_filestate.config_option.type_config_option import \
