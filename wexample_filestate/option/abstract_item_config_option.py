2b6c942f66ab9e41fe4a3256e71f1e7378fa495e
weeger <romain.weeger@gmail.com>
2024-10-31 12:38:16 +0100

    Dead code

--- wexample_filestate/option/abstract_item_config_option.py
+++
@@ -1,9 +0,0 @@
-from typing import Type
-
-from wexample_config.option.abstract_config_option import AbstractConfigOption
-from wexample_filestate.config_value.item_config_value import ItemConfigValue
-
-
-class AbstractItemConfigOption(AbstractConfigOption):
-    def get_value_class_type(self) -> Type:
-        return ItemConfigValue
