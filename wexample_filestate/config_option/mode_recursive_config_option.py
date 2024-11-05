ff73549fc7ffe83c8816a6e93c4804603edce87c
weeger <romain.weeger@gmail.com>
2024-11-05 07:56:00 +0100

    Mode typing

--- wexample_filestate/config_option/mode_recursive_config_option.py
+++ wexample_filestate/config_option/mode_recursive_config_option.py
@@ -1,12 +1,10 @@
-from typing import Any
+from typing import Any, Union
 
 from wexample_config.config_option.abstract_config_option import AbstractConfigOption
 from wexample_filestate.const.files import FileSystemPermission
 
 
 class ModeRecursiveConfigOption(AbstractConfigOption):
-    value: FileSystemPermission
-
     @staticmethod
     def get_raw_value_allowed_type() -> Any:
-        return bool
+        return Union[bool, FileSystemPermission]
