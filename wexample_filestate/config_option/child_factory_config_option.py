57e323d84b6dc50fc735020dea4035209fc6c7ae
weeger <romain.weeger@gmail.com>
2024-11-06 08:02:51 +0100

    Support multiple patterns

--- wexample_filestate/config_option/child_factory_config_option.py
+++ wexample_filestate/config_option/child_factory_config_option.py
@@ -7,6 +7,7 @@ from typing import Any, TYPE_CHECKING, cast, List
 from wexample_config.config_option.abstract_nested_config_option import AbstractNestedConfigOption
 from wexample_config.const.types import DictConfig
 from wexample_filestate.config_option.mixin.item_config_option_mixin import ItemTreeConfigOptionMixin
+from wexample_filestate.config_option.name_pattern_config_option import NamePatternConfigOption
 
 if TYPE_CHECKING:
     from wexample_config.options_provider.abstract_options_provider import AbstractOptionsProvider
@@ -36,21 +37,35 @@ class ChildFactoryConfigOption(ItemTreeConfigOptionMixin, AbstractNestedConfigOp
         config = self.pattern
 
         children = []
-        if config.get("name_pattern"):
-            path = self.get_parent_item().get_path()
+        name_pattern_option_name = NamePatternConfigOption.get_name()
+        parent_item = self.get_parent_item()
+        if config.get(name_pattern_option_name):
+            path = parent_item.get_path()
             if path.exists():
-                base_path = self.get_parent_item().get_resolved()
-                pattern = re.compile(config["name_pattern"])
+                base_path = parent_item.get_resolved()
+
+                patterns = config[name_pattern_option_name]
+                if isinstance(patterns, str):
+                    patterns = [patterns]
                 for file in os.listdir(base_path):
-                    if pattern.match(file):
+                    all_match = True
+                    for pattern_str in patterns:
+                        pattern = re.compile(pattern_str)
+                        if not pattern.match(file):
+                            all_match = False
+                            break
+
+                    if all_match:
                         path = Path(f"{base_path}{file}")
 
                         if "type" not in config or config_has_same_type_as_file(config, path):
                             item_config_copy = copy.deepcopy(config)
 
+                            if item_config_copy.get("name", None) is None:
+                                item_config_copy["name"] = path.name
+
                             children.append(parent_children_config.create_child_item(
                                 child_config=item_config_copy,
-                                item_name=path.name,
                             ))
 
         return children
