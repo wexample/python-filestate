449c4ed33f3938eab15d27ca1194c7a56eac871d
weeger <romain.weeger@gmail.com>
2024-11-06 10:25:22 +0100

    Config

--- wexample_filestate/config_option/children_filter_config_option.py
+++ wexample_filestate/config_option/children_filter_config_option.py
@@ -14,9 +14,7 @@ if TYPE_CHECKING:
 class ChildrenFilterConfigOption(AbstractChildrenManipulationConfigOption):
     def generate_children(self) -> List["TargetFileOrDirectoryType"]:
         from wexample_filestate.helpers.config_helper import config_has_same_type_as_file
-
         config = self.pattern
-
         children = []
 
         name_pattern_option_name = NamePatternConfigOption.get_name()
@@ -25,11 +23,9 @@ class ChildrenFilterConfigOption(AbstractChildrenManipulationConfigOption):
             path = parent_item.get_path()
             if path.exists():
                 base_path = parent_item.get_resolved()
-                if self._path_match_patterns(base_path):
-                    for file in os.listdir(base_path):
-                        path = Path(f"{base_path}{file}")
-
+                for file in os.listdir(base_path):
+                    path = Path(f"{base_path}{file}")
+                    if self._path_match_patterns(str(path)):
                         if "type" not in config or config_has_same_type_as_file(config, path):
                             children.append(self._create_children_from_config(path, config))
-
         return children
