c21284ea6447ac4b8d869f356939fb475a9392c6
weeger <romain.weeger@gmail.com>
2025-08-23 09:30:04 +0200

    Toml file

--- wexample_filestate/item/file/toml_file.py
+++ wexample_filestate/item/file/toml_file.py
@@ -4,7 +4,6 @@ from typing import ClassVar
 
 from wexample_filestate.item.file.structured_content_file import StructuredContentFile
 from wexample_helpers.const.types import StructuredData
-from wexample_helpers_toml.const.types import TomlContent  # type: ignore
 
 
 class TomlFile(StructuredContentFile):
@@ -14,8 +13,6 @@ class TomlFile(StructuredContentFile):
         return self.EXTENSION_TOML
 
     def _parse_file_content(self, content: str) -> StructuredData:  # type: ignore[name-defined]
-        # Prefer the widely used 'toml' package for both load and dump symmetry.
-        # If you prefer stdlib reading, replace with 'tomllib' and pair with 'tomli_w' for writing.
         import toml
 
         try:
