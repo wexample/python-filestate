2558ae447ef5a6a919c8fcb400e28baf8556ff72
weeger <romain.weeger@gmail.com>
2025-08-25 10:55:10 +0200

    search

--- src/wexample_filestate/search/search_result.py
+++ src/wexample_filestate/search/search_result.py
@@ -1,11 +1,20 @@
 from pydantic import Field
 
-from wexample_filestate.const.state_items import TargetFileOrDirectory
+from wexample_filestate.item.item_target_file import ItemTargetFile
 from wexample_helpers.classes.extended_base_model import ExtendedBaseModel
 
 
 class SearchResult(ExtendedBaseModel):
-    item: TargetFileOrDirectory = Field()
+    item: ItemTargetFile = Field()
     searched: str = Field()
     line: int = Field()
     column: int = Field()
+
+    @classmethod
+    def create_if_match(cls, search: str, item: ItemTargetFile) -> SearchResult | None:
+        if search in item.read():
+            return cls(
+                item=item,
+                # TODO line and col
+            )
+        return None
