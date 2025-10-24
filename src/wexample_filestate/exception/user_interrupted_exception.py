840b55499549cd5cd9bcf49c295cdc536738bcf7
weeger <romain.weeger@gmail.com>
2025-10-24 18:28:19 +0200

    Configure

--- src/wexample_filestate/exception/user_interrupted_exception.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from wexample_helpers.exception.undefined_exception import UndefinedException
-
-
-class UserInterruptedException(UndefinedException):
-    error_code: str = "USER_INTERRUPTED_EXCEPTION"
