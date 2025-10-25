f7bb61c2da51ec1192557123dddbb5bd050f6f76
weeger <romain.weeger@gmail.com>
2025-10-25 18:41:59 +0200

    Minor

--- src/wexample_filestate/result/file_state_result.py
+++ src/wexample_filestate/result/file_state_result.py
@@ -26,7 +26,7 @@ class FileStateResult(AbstractResult):
             if self.state_manager.io.confirm(
                 question=f"Do you want to apply this change:\n"
                 f"    {operation.target.get_item_title()}: {operation.target.render_display_path()}\n"
-                f"    Option: {cli_make_clickable_path(classes_get_definition_path(operation.option), operation.option.get_name())} - {operation.option.get_description()}\n"
+                f"    Option: {cli_make_clickable_path(classes_get_definition_path(operation.option), operation.option.get_name())}\n"
                 f"  → {operation.description}\n",
                 choices=ConfirmPromptResponse.MAPPING_PRESET_YES_NO,
                 default="yes",
