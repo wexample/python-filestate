5b474f848d8b11ed107ac67c0d00de458b8bca10
weeger <romain.weeger@gmail.com>
2025-10-25 14:33:18 +0200

    Various

--- src/wexample_filestate/item/abstract_item_target.py
+++ src/wexample_filestate/item/abstract_item_target.py
@@ -112,7 +112,7 @@ class AbstractItemTarget(
                 if applied_operations:
                     self.operations_history.append(applied_operations)
             else:
-                self.info(
+                self.io.info(
                     message=f"No operation to execute on: {cli_make_clickable_path(self.get_path())} ",
                 )
         except KeyboardInterrupt:
