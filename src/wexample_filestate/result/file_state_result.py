32c0ad90616c8f089e5b7b242380197fbfe8e2e3
weeger <romain.weeger@gmail.com>
2025-10-25 18:24:28 +0200

    Prompt

--- src/wexample_filestate/result/file_state_result.py
+++ src/wexample_filestate/result/file_state_result.py
@@ -14,19 +14,19 @@ class FileStateResult(AbstractResult):
     def _apply_single_operation(
         self, operation: AbstractOperation, interactive: bool = False
     ) -> bool:
+        from wexample_helpers.helpers.classes import classes_get_definition_path
         from wexample_prompt.responses.interactive.confirm_prompt_response import (
             ConfirmPromptResponse,
         )
 
         if interactive:
-            import inspect
             import os.path
             from wexample_helpers.helpers.cli import cli_make_clickable_path
 
             if self.state_manager.io.confirm(
                 question=f"Do you want to apply this change:\n"
                 f"    {operation.target.get_item_title()}: {operation.target.render_display_path()}\n"
-                f"    Option: {cli_make_clickable_path(inspect.getfile(operation.option), operation.option.get_name())} - {operation.option.get_description()}\n"
+                f"    Option: {cli_make_clickable_path(classes_get_definition_path(operation.option), operation.option.get_name())} - {operation.option.get_description()}\n"
                 f"  → {operation.description}\n",
                 choices=ConfirmPromptResponse.MAPPING_PRESET_YES_NO,
                 default="yes",
