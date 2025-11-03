c64de97db3a8b52def7ca6f86abb22f6bdd880d1
weeger <romain.weeger@gmail.com>
2025-11-03 21:54:00 +0100

    Publishing version 0.0.56

--- src/wexample_filestate/item/mixins/item_mixin.py
+++ src/wexample_filestate/item/mixins/item_mixin.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_helpers.mixin.with_path_mixin import WithPathMixin
 from wexample_helpers.classes.abstract_method import abstract_method
 from wexample_helpers.classes.field import public_field
 from wexample_helpers.decorator.base_class import base_class
+from wexample_helpers.mixin.with_path_mixin import WithPathMixin
 
 if TYPE_CHECKING:
     from wexample_helpers.const.types import FileStringOrPath
