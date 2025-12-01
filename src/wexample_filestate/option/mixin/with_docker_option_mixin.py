1d12184fffb3498c05f4672ddeb5d6b3dc3e247b
weeger <romain.weeger@gmail.com>
2025-12-01 18:20:30 +0100

    PhpCs option

--- src/wexample_filestate/option/mixin/with_docker_option_mixin.py
+++ src/wexample_filestate/option/mixin/with_docker_option_mixin.py
@@ -1,2 +1,34 @@
+from typing import TYPE_CHECKING
+
+from wexample_filestate.const.types_state_items import TargetFileOrDirectoryType
+
+if TYPE_CHECKING:
+    from wexample_filestate.const.types_state_items import TargetFileOrDirectoryType
+
+
 class WithDockerOptionMixin:
-    pass
+
+    def _ensure_docker_container(self) -> None:
+        from wexample_helpers.helpers.shell import shell_run
+
+        shell_run(
+            cmd=[
+                # TODO
+                "docker",
+                "ps"
+            ],
+            inherit_stdio=True
+        )
+
+    def _execute_in_docker(self, target: TargetFileOrDirectoryType, command: str):
+        from wexample_helpers.helpers.shell import shell_run
+
+        self._ensure_docker_container()
+
+        shell_run(
+            cmd=[
+                "echo",
+                command
+            ],
+            inherit_stdio=True
+        )
