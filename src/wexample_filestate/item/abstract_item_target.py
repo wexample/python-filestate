fba02da73a9b3eaf9e228d4eabd7ce5e36a6bf6f
weeger <romain.weeger@gmail.com>
2025-12-05 08:19:53 +0100

    Symfony

--- src/wexample_filestate/item/abstract_item_target.py
+++ src/wexample_filestate/item/abstract_item_target.py
@@ -101,17 +101,20 @@ class AbstractItemTarget(
         filter_path: str | None = None,
         filter_operation: str | None = None,
         max: int = None,
+        result: FileStateResult | None = None
     ) -> FileStateResult:
         from wexample_filestate.enum.scopes import Scope
         from wexample_filestate.result.file_state_result import FileStateResult
 
-        result = FileStateResult(state_manager=self)
+        result = result or FileStateResult(state_manager=self)
+
+        if scopes is None:
+            scopes = set(Scope)
+
+
+        self.last_result = result
 
         try:
-            if scopes is None:
-                scopes = set(Scope)
-
-            self.last_result = result
             self.build_operations(
                 result=result,
                 scopes=scopes,
